97bbb60f3dc1d1f5dcacc47b273dedbf0bc68a53
Dzale <ndzakovic@yahoo.com>
2023-05-09 13:20:31 +0200

    WIP

--- app/tests.py
+++ app/tests.py
@@ -5,8 +5,8 @@ from app import calc
 class CalcTest(SimpleTestCase):
     def test_add_should_return_correct_result(self):
         res = calc.add(5, 6)
-        self.assertEquals(res, 11)
+        self.assertEqual(res, 11)
 
     def test_sub_should_return_correct_result(self):
         res = calc.sub(8, 6)
-        self.assertEquals(res, 2)
+        self.assertEqual(res, 2)
